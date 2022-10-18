96c12fb853171dac6fbf5fa65141fe4f49c19f88
haneunjii <106488799+haneunjii@users.noreply.github.com>
2022-10-19 05:26:15 +0900

    Add files via upload

---
+++ login_page.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace supermarket.Display
+{
+    partial class login_page
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx_name = new System.Windows.Forms.TextBox();
+            this.tx_id = new System.Windows.Forms.TextBox();
+            this.tx_pw = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_register = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tx_name
+            // 
+            this.tx_name.Location = new System.Drawing.Point(293, 125);
+            this.tx_name.Name = "tx_name";
+            this.tx_name.Size = new System.Drawing.Size(200, 21);
+            this.tx_name.TabIndex = 18;
+            // 
+            // tx_id
+            // 
+            this.tx_id.Location = new System.Drawing.Point(293, 153);
+            this.tx_id.Name = "tx_id";
+            this.tx_id.Size = new System.Drawing.Size(200, 21);
+            this.tx_id.TabIndex = 19;
+            // 
+            // tx_pw
+            // 
+            this.tx_pw.Location = new System.Drawing.Point(293, 181);
+            this.tx_pw.Name = "tx_pw";
+            this.tx_pw.Size = new System.Drawing.Size(200, 21);
+            this.tx_pw.TabIndex = 20;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(293, 228);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(116, 52);
+            this.btn_login.TabIndex = 21;
+            this.btn_login.Text = "로그인";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // btn_register
+            // 
+            this.btn_register.Location = new System.Drawing.Point(293, 300);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(116, 52);
+            this.btn_register.TabIndex = 22;
+            this.btn_register.Text = "회원가입";
+            this.btn_register.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "아이디";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "비밀번호";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 12);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "로그인을 해주세요";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_register);
+            this.panel1.Controls.Add(this.btn_login);
+            this.panel1.Controls.Add(this.tx_pw);
+            this.panel1.Controls.Add(this.tx_id);
+            this.panel1.Controls.Add(this.tx_name);
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 546);
+            this.panel1.TabIndex = 0;
+            // 
+            // login_page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "login_page";
+            this.Size = new System.Drawing.Size(878, 552);
+            this.Load += new System.EventHandler(this.login_page_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx_name;
+        private System.Windows.Forms.TextBox tx_id;
+        private System.Windows.Forms.TextBox tx_pw;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_register;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ login_page.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+
+
+namespace supermarket.Display
+{
+    public partial class login_page : UserControl
+    {
+        Display.flatform_main fl = new Display.flatform_main();
+        public login_page()
+        {
+            InitializeComponent();
+        }
+        private void login_page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(fbc);
+            }
+            catch
+            {
+                MessageBox.Show("문제발생");
+            }
+        }
+        FirebaseConfig fbc = new FirebaseConfig()
+        {
+            AuthSecret = "AIzaSyC8FDbyONe9rEpZ88g_MDfZ9jp5HjfORXU",
+            BasePath = "https://userdata-64f98-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+
+        private void btn_register_Click(object sender, EventArgs e)
+        {
+            var result = client.Get("가입자 명단/" + tx_id.Text);
+            upload upd1 = result.ResultAs<upload>();
+            upload upd2 = new upload()
+            {
+                Name = tx_name.Text,
+                id = tx_id.Text,
+                pw = tx_pw.Text
+            };
+
+            var send = client.Set("가입자 명단/" + tx_id.Text, upd2);
+            MessageBox.Show("회원가입 완료");
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            var result = client.Get("가입자 명단/" + tx_id.Text);
+            dawnload upd1 = result.ResultAs<dawnload>(); //데이버베이스레서 가져온 정보 다운로드 클래스에 저장        
+            upload upd2 = new upload()  //폼에서 입력한 정보 업로드 틀레스에 저장
+            {
+                Name = tx_name.Text,
+                id = tx_id.Text,
+                pw = tx_pw.Text
+            };
+
+            if (tx_id.Text != "" | tx_name.Text != "" | tx_pw.Text !="") //이력란에 아무것도 입력이 안되어 있는 경우
+            {
+                MessageBox.Show("정보를 입력해 주세요");
+            }
+            else if(tx_id.Text != "" && tx_name.Text != "" && tx_pw.Text != "") //입력란에 입력이 되있는 경우
+            {
+                
+                if(tx_id.Text != upd1.id && tx_name.Text != upd1.Name && tx_pw.Text != upd1.pw) //가입이 안된 직원인 경우
+                {
+                    MessageBox.Show("가입이 안된 직원입니다");
+                }
+                else      //가입이 되어있는 경우
+                {
+                    MessageBox.Show("로그인 되었습니다");
+                    panel1.Controls.Clear();
+                    //가입된 직원일경우
+                }
+            }
+        }
+    }
+}
